9aab17abb29ad85797731606c39f76de7edeec3c
AnaRitaRangel <106035404+AnaRitaRangel@users.noreply.github.com>
2025-03-25 16:49:43 -0400

    Projeto aula 17

---
+++ class-17/exemplo_01.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+
+# Conectar ao SQLite em memória
+engine = create_engine('sqlite:///meubanco.db', echo=True)
+
+engine = create_engine("postgresql+psycopg2://scott:tiger@localhost:5432/mydatabase")
+
+print("Conexão com SQLite estabelecida.")
